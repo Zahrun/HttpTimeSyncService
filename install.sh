1d274fd55e07ad27bb130b7621172094048ad6fa
Zahrun <zahrun@murena.io>
2023-09-25 11:22:23 +0530

    rename to daemon

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-sudo cp http_time_sync.sh http_time_sync_service.sh /usr/bin/
+sudo cp http_time_sync.sh http_time_syncd.sh /usr/bin/
 sudo cp HttpTimeSyncService.service /etc/systemd/system/
 
 echo $0 finished, press enter to continue
