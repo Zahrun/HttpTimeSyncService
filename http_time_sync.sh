702d0697011c4cfc0bc7a0a80324a7eb8ef44624
Zahrun <zahrun@murena.io>
2023-09-25 11:43:56 +0530

    start service

--- http_time_sync.sh
+++ http_time_sync.sh
@@ -2,4 +2,4 @@
 
 httpdate="$(wget --no-cache -S -O /dev/null google.com 2>&1 |
         sed -n -e 's/  *Date: *//p' -eT -eq)"
-[ -n "$httpdate" ] && sudo date -s "$httpdate"
+[ -n "$httpdate" ] && date -s "$httpdate"
--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@ cd "$(dirname "$0")"
 
 sudo cp http_time_sync.sh http_time_syncd.sh /usr/bin/
 sudo cp HttpTimeSyncService.service /etc/systemd/system/
+sudo systemctl enable --now HttpTimeSyncService
+systemctl status HttpTimeSyncService
 
 echo $0 finished
