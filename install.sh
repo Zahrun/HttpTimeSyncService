dbdafc4b6a8b49c979e3b7b11384fafe23d1d3e4
Zahrun <zahrun@murena.io>
2023-09-25 11:23:11 +0530

    install script ends

--- install.sh
+++ install.sh
@@ -5,5 +5,4 @@ cd "$(dirname "$0")"
 sudo cp http_time_sync.sh http_time_syncd.sh /usr/bin/
 sudo cp HttpTimeSyncService.service /etc/systemd/system/
 
-echo $0 finished, press enter to continue
-read
+echo $0 finished
