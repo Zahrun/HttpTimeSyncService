1e723bcafa6af375ff2a580661fb5794ded818e9
Zahrun <zahrun@murena.io>
2023-10-08 21:53:46 +0530

    added slower sync after first sync success

--- http_time_syncd.sh
+++ http_time_syncd.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
-while (true); do
-        /usr/bin/http_time_sync.sh
+synced=false
+while (! ${synced}); do
+        /usr/bin/http_time_sync.sh && synced=true || echo sync failed
         sleep 10
+done
+while (true); do
+	/usr/bin/http_time_sync.sh
+	sleep 60
 done
